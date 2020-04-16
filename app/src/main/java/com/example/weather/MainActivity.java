e52b5d76c48653148d9c65e46baa6f1250060946
FatED42 <cjkrol42@gmail.com>
2020-04-16 17:09:20 +0300

    Изменил размер текста в спинере

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -14,9 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.cities_choise);
 
         initViews();
+
     }
 
     private void initViews() {
+        Spinner cities_spinner = findViewById(R.id.cities_spinner);
 
+        ArrayAdapter<CharSequence> citiesAdapter = ArrayAdapter.createFromResource(
+                this,R.array.cities, R.layout.spinner_layout);
+        citiesAdapter.setDropDownViewResource(R.layout.spinner_layout);
+        cities_spinner.setAdapter(citiesAdapter);
     }
 }
