b9a8af8b2abc1255452582045c8294aca3870bf3
FatED42 <cjkrol42@gmail.com>
2020-04-23 18:33:07 +0300

    Связал две активити

---
+++ app/src/main/java/com/example/weather/Parcel.java
@@ -0,0 +1,9 @@
+package com.example.weather;
+
+import java.io.Serializable;
+
+public class Parcel implements Serializable {
+    String city = "";
+    Boolean windSpeed = false;
+    boolean pressure = false;
+}
